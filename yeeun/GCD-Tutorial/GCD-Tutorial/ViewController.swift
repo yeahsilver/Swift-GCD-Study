a95c834822d6558893394708ae1cdfc0ee72657d
yeahsilver <gjdpdms2005@gmail.com>
2022-06-07 22:59:17 -0400

    feat: 코드 형상화

--- yeeun/GCD-Tutorial/GCD-Tutorial/ViewController.swift
+++ yeeun/GCD-Tutorial/GCD-Tutorial/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
+let chapters = [1, 2, 3, 4, 5]
+
 class ViewController: UIViewController {
+    // MARK: Properties
+    private var dataSource: UITableViewDiffableDataSource<Int, UUID>!
+    private var snapshot = NSDiffableDataSourceSnapshot<Int, UUID>()
+    
+    // MARK: Components
+    private lazy var tableView: UITableView = { createTableView() }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initTableView()
+        initSnapshot()
+        
+        style()
+        layout()
     }
-
+    
+    private func initTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        dataSource = UITableViewDiffableDataSource<Int, UUID>(tableView: tableView, cellProvider: { tableView, indexPath, itemIdentifier in
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+            
+            return cell
+        })
+        
+        tableView.dataSource = dataSource
+    }
+    
+    private func initSnapshot() {
+        snapshot.appendSections([0])
+        
+        snapshot.appendItems(chapters)
+        dataSource.apply(snapshot)
+    }
 
 }
 
+extension ViewController {
+    private func createTableView() -> UITableView {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }
+}
+
+extension ViewController {
+    private func style() {
+        view.addSubview(tableView)
+    }
+    
+    private func layout(){
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+}
